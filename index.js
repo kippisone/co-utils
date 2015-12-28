80b24545b7d694588933890d1e1d58b740638bc6
AndiOxidant <andi.oxidant@noname-media.com>
2015-12-28 13:18:19 +0100

    Support functions who returning promises

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ var co = require('co');
 
 module.exports = co;
 module.exports.series = function(arr) {
+    var promiseFn = function(fn) {
+        return function(resolve, reject) {
+            let p2 = fn(reject, resolve);
+            if (p2 && p2.then && p2.catch) {
+                p2.then(resolve).catch(reject);
+            }
+        };
+    };
+    
     return co(function* () {
         var result = [];
         for (let promise of arr) {
-            let res = yield promise;
+            let res;
+            if (typeof promise === 'function') {
+                res = yield new Promise(promiseFn(promise));
+
+            }
+            else {
+                res = yield promise;
+            }
+
             result.push(res);
         }
 
